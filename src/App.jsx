15f968bd1611f592f76211e4f0e0023c5781815a
svivek19 <svivek030503@gmail.com>
2024-02-25 17:17:32 +0530

    ui change

--- src/App.jsx
+++ src/App.jsx
@@ -46,18 +46,18 @@ const App = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-100 p-8 rounded-lg sm:w-1/2 md:w-1/3">
+      <div className="bg-transparent backdrop-blur-2xl p-8 rounded-lg sm:w-1/2 md:w-1/3">
         <div className='flex justify-center'>
           <img src="https://ofpfunding-1fcf3.kxcdn.com/wp-content/uploads/2023/12/Currency-Converter-2.png" alt="image" className='w-1/2' />
         </div>
-        <h1 className="text-3xl md:text-4xl text-violet-950 text-center border-dashed border-t-2 border-b-2 border-purple-400 font-bold mb-4">Currency Converter</h1>
+        <h1 className="text-3xl md:text-4xl text-violet-100 text-center border-dashed border-t-2 border-b-2 border-purple-400 font-bold mb-4">Currency Converter</h1>
         <div className="mb-4">
-          <label htmlFor="amount" className='font-semibold'>Amount: </label>
-          <input type="number" id="amount" className="block w-full font-semibold p-2 border border-gray-300 rounded-md focus:ring-violet-700 outline-none focus:border-violet-700" value={amount} onChange={handleAmountChange} />
+          <label htmlFor="amount" className='font-semibold text-violet-100'>Amount: </label>
+          <input type="number" id="amount" className="block w-full font-semibold p-2 border border-gray-300 rounded-md focus:ring-violet-700 outline-none bg-violet-300 focus:border-violet-700" value={amount} onChange={handleAmountChange} />
         </div>
         <div className="mb-4">
-          <label htmlFor="fromCurrency" className='font-semibold'>From Currency: </label>
-          <select id="fromCurrency" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 outline-none focus:border-indigo-500 font-medium p-2 border" value={fromCurrency} onChange={handleFromCurrencyChange}>
+          <label htmlFor="fromCurrency" className='font-semibold text-violet-100'>From Currency: </label>
+          <select id="fromCurrency" className="block w-full bg-violet-300 border-gray-300 rounded-md focus:ring-indigo-500 outline-none focus:border-indigo-500 font-medium p-2 border" value={fromCurrency} onChange={handleFromCurrencyChange}>
             <option value="USD">USD - United States Dollar</option>
             <option value="EUR">EUR - Euro</option>
             <option value="GBP">GBP - British Pound Sterling</option>
@@ -71,8 +71,8 @@ const App = () => {
           </select>
         </div>
         <div className="mb-4">
-          <label htmlFor="toCurrency" className='font-semibold'>To Currency: </label>
-          <select id="toCurrency" className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 outline-none focus:border-indigo-500 font-medium p-2 border" value={toCurrency} onChange={handleToCurrencyChange}>
+          <label htmlFor="toCurrency" className='font-semibold text-violet-100'>To Currency: </label>
+          <select id="toCurrency" className="block w-full bg-violet-300 border-gray-300 rounded-md focus:ring-indigo-500 outline-none focus:border-indigo-500 font-medium p-2 border" value={toCurrency} onChange={handleToCurrencyChange}>
             <option value="USD">USD - United States Dollar</option>
             <option value="EUR">EUR - Euro</option>
             <option value="GBP">GBP - British Pound Sterling</option>
@@ -85,7 +85,7 @@ const App = () => {
             <option value="ZAR">ZAR - South African Rand</option>
           </select>
         </div>
-        <div className='border text-center rounded-lg mt-8 border-violet-500 font-bold text-violet-500 border-dashed p-2'>
+        <div className='border text-center rounded-lg mt-8 border-violet-300 font-bold text-violet-300 border-dashed p-2'>
           <p>{amount} {fromCurrency} is equal to {convertedAmount} {toCurrency}</p>
         </div>
       </div>
